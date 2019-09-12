8ac12c292d6f70e63fb51c8ee9ba4ed303578466
zzb001 <zzb@163.com>
2019-09-12 11:32:38 +0800

    Initial commit

---
+++ scripts/test01.py
@@ -0,0 +1,10 @@
+class Test01:
+    def test001(self):
+        print("test001被执行")
+
+    def test002(self):
+        print("test002被执行")
+
+    def test003(self):
+        print("test003被执行")
+        assert 0
